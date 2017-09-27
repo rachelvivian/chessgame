d5e06a5984eae1fbc3f62a678e6b4a50373d929c
Rachel <rachviv@gmail.com>
2017-09-27 02:35:35 +0000

    created chess board

--- src/js/initBoard.js
+++ src/js/initBoard.js
@@ -1,8 +1,24 @@
 function initBoard () {
     console.log('initBoard');
+    const board = document.getElementById('chess-board');
     
-    for(let i = 0;i < 8;i++) {
+    for(let x = 0;x < 8;x++) {
         const row = document.createElement('div');
+        row.classList.add('row');
+        for(let y = 0;y < 8;y++) {
+            const space = document.createElement('div');
+            if (x % 2 === 0) {
+                y % 2 === 0 ?
+                space.classList.add('white-space') :
+                space.classList.add('black-space');
+            } else {
+                y % 2 === 0 ?
+                space.classList.add('black-space') :
+                space.classList.add('white-space');
+            }
+            row.appendChild(space);
+        }
+        board.appendChild(row);
     }
     
 }
